129a3cb6a59374737aae2b0f07d99048c4d8f66b
kamenoseiji <seiji.kameno@nao.ac.jp>
2014-09-05 12:21:44 +0900

    Modified method to determine delay. Added generation of complex bandpass table.
    	modified:   delaysearch.R

--- delaysearch.R
+++ delaysearch.R
@@ -7,9 +7,12 @@ corr2spec <- function( corr ){
 #-------------------------------------- Function to calculate correlation function from cross spectrum
 spec2corr <- function(spec){
 	nspec <- length(spec)
-	tmpspec <- c(spec, 0, Conj(spec[nspec:2]))
-	corr <- Re(fft(tmpspec, inverse=TRUE) / nspec)
-	return(c(corr[(nspec+1):(2*nspec)], corr[1:nspec]))
+	tempcorr <- fft( c(spec, rep(0.0, nspec)), inverse=T)
+	corr <- c(tail(tempcorr, nspec), head(tempcorr, nspec))
+	return(corr)
+	# tmpspec <- c(spec, 0, Conj(spec[nspec:2]))
+	# corr <- Re(fft(tmpspec, inverse=TRUE) / nspec)
+	# return(c(corr[(nspec+1):(2*nspec)], corr[1:nspec]))
 }
 
 #-------------------------------------- Delay Search
@@ -17,9 +20,9 @@ delay_search <- function( spec ){
 	nspec <- length( spec )
 	#-------- Search for delay
 	delay <- which.max(Mod(spec2corr(spec))) - nspec - 1	# Coarse Delay
-	trial_delay <- delay + seq(-1, 1, by=0.5); trial_amp <- numeric(0)
-	for(i in 1:5){ trial_amp[i] <- Mod(sum(delay_cal(spec, trial_delay[i]))) }
-	fit <- lm( formula = trial_amp ~ trial_delay + I(trial_delay^2) )
+	trial_delay <- delay + seq(-1, 1, by=0.1); trial_amp <- numeric(0)
+	for(i in 1:length(trial_delay)){ trial_amp[i] <- Mod(sum(delay_cal(spec, trial_delay[i]))) }
+	fit <- lm( formula = trial_amp ~ trial_delay + I(trial_delay^2))
 	return(-fit[[1]][[2]] / (2.0* fit[[1]][[3]]))
 }
 
@@ -39,3 +42,20 @@ delayPhase_cal <- function( spec, delay, phase ){
 	twiddle <- complex(modulus=rep(1, nspec), argument = phase + delay* pi* seq((-nspec/2), (nspec/2 - 1), by=1) / nspec)
 	return( spec* twiddle )
 }
+
+#-------------------------------------- Complex Bandpass Table
+BPtable <- function( spec, smoothCH ){
+	delay <- delay_search(spec)
+	delCaledSpec <- delay_cal(spec, delay)
+	return(smoothComplex(delCaledSpec, smoothCH))
+}
+
+smoothComplex <- function( spec, smoothCH ){
+	nspec <- length(spec)
+	reSpec <- predict(smooth.spline( Re(spec), all.knots=F, nknots=as.integer(nspec/smoothCH)), 1:length(spec))$y
+	imSpec <- predict(smooth.spline( Im(spec), all.knots=F, nknots=as.integer(nspec/smoothCH)), 1:length(spec))$y
+	return(complex(real=reSpec, imaginary=imSpec))
+}
+
+
+
