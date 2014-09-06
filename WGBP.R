61a270a8d73ce6c4915db367cb5d5e3ac2e063a5
kamenoseiji <seiji.kameno@nao.ac.jp>
2014-09-06 23:01:55 +0900

    new file:   WGBP.R 	modified:   WGScan.R

---
+++ WGBP.R
@@ -0,0 +1,59 @@
+# WGBP.R : Generate complex bandpass table using WG scan
+# usage: Rscript WGBP.R [thresh] [prefix list]
+# e.g. Rscript WGBP.R 15 2014107010610 2014107013610 2014107020610 2014107023610 2014107030610 2014107033610 2014107040610 2014107043610 2014107050610 2014107053610 2014107060610 2014107063610 2014107070610 2014107073610 2014107080610 2014107083610 2014107090610
+ 
+#
+library(RCurl)
+eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/readPolariS.R", ssl.verifypeer = FALSE)))
+eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/date.R", ssl.verifypeer = FALSE)))
+setwd('.')
+
+#-------- Function to filter WG scans
+scanGap <- function( XP, thresh, gapThresh ){
+	WG_index <- which(Mod(XP$C00) > thresh)	# filter WG scans
+	scanGap <- which( diff(WG_index) > gapThresh)
+	scanEnd <- c(WG_index[scanGap], max(WG_index))
+	scanStart <- c(min(WG_index), WG_index[scanGap+1])
+	scanNum <- length(scanStart); XPamp <- numeric(0)
+	for(scan_index in 1:scanNum){
+		scanRange <- scanStart[scan_index]:scanEnd[scan_index]
+		XPamp[scan_index] <- Mod( mean(XP$C00[scanRange]) + mean(XP$C00[scanRange]) )
+	}
+	return(data.frame( startMJD=XP$mjdSec[scanStart], endMJD=XP$mjdSec[scanEnd], XPamp = XPamp))
+}
+
+#-------- Function to pick prefix that covers specified MJD
+findPrefix <- function(mjdSec, prefix){
+	mjdPrefix <- as.numeric(lapply(prefix, FUN=prefix2MJDsec))
+	index <- which(mjdPrefix <= mjdSec)
+	if( length(index) == 0){	return(-1)}
+	return(max(index))
+}
+
+#-------- Function to Generate BP table
+makeBP <- function(scanXP, prefix){
+	BPscanIndex <- which.max(scanXP$XPamp)
+	file_index <- findPrefix(scanXP$startMJD[BPscanIndex], prefix)
+	endPoint   <- min( c(scanXP$endMJD[BPscanIndex] - prefix2MJDsec(prefix[file_index]) + 1, 1800) )
+	startPoint <- scanXP$startMJD[BPscanIndex] - prefix2MJDsec(prefix[file_index]) + 1
+	C00 <- apply(readPolariS_X(sprintf('%s.C.%02d', prefix[file_index], 0))[,startPoint:endPoint], 1, mean)
+	C01 <- apply(readPolariS_X(sprintf('%s.C.%02d', prefix[file_index], 1))[,startPoint:endPoint], 1, mean)
+	BP <- data.frame( BP00 = BPtable(C00, length(C00)/6), BP01 =  BPtable(C01, length(C01)/6) )
+	return(BP)
+}
+
+#-------- Procedures
+args <- commandArgs()
+thresh <- as.numeric(args[6])
+prefix <- args[7:length(args)]
+XPfname <- sprintf('%s.XP.Rdata', prefix[1])
+
+#-------- Load XP and identify WG scans
+cat(XPfname); cat('\n')
+load(XPfname)
+scanXP <- scanGap(XP, thresh, 60 )
+
+#-------- Generate Complex BP table
+BP <- makeBP(scanXP, prefix)
+save( BP, scanXP, file=sprintf("%s.BP.Rdata", prefix[1]))
+
--- WGScan.R
+++ WGScan.R
@@ -1,24 +1,22 @@
 # WGScan.R
-# usage: Rscript WGScan.R thresh [list of prefix]
+# usage: Rscript WGScan.R [list of prefix]
 # prefix is YYYYDOYHHMMSS in the PolariS file name (e.g. 2013362105803)
 #
 library(RCurl)
 eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/readPolariS.R", ssl.verifypeer = FALSE)))
-# eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/readSAM45.R", ssl.verifypeer = FALSE)))
-# eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/Qeff.R", ssl.verifypeer = FALSE)))
-# eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/findSAM45Log.R", ssl.verifypeer = FALSE)))
-
+eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/date.R", ssl.verifypeer = FALSE)))
+setwd('.')
 #-------- Function to find Scan Pattern from SAM45 Log
 scanXP <- function(prefix){
 	PolarisFileNum <- length(prefix)
+	C00 <- numeric(0); mjdSec <- numeric(0)
 	for(index in 1:PolarisFileNum){
 		cat( sprintf("Loading %s ….\n", prefix[index]))
-		C00 <- numeric(0); C01 <- numeric(0); mjdSec <- numeric(0)
-		C00 <- append(C00, apply(Mod(apply(readPolariS_X(sprintf('%s.C.%02d', prefix[1], 0)), 2, fft)), 2, max))
-		C01 <- append(C01, apply(Mod(apply(readPolariS_X(sprintf('%s.C.%02d', prefix[1], 1)), 2, fft)), 2, max))
-		mjdSec <- append(mjdSec, (prefix2MJDsec(prefix[index]) + seq(0, length(C00)-1, by=1)))
+		temp <- readPolariS_X(sprintf('%s.C.%02dB', prefix[index], 0))
+		C00 <- append(C00, apply(Mod(apply(temp, 2, fft)), 2, max))
+		mjdSec <- append(mjdSec, (prefix2MJDsec(prefix[index]) + seq(0, dim(temp)[2]-1, by=1)))
 	}
-	return(data.frame(mjdSec, C00/chnum, C01/chnum))
+	return(data.frame(mjdSec, C00))
 }
 
 #-------- Function to plot Amplitude and Phase in a page
@@ -35,12 +33,9 @@ amphi_plot <- function(Time, Vis, label){
 
 #-------- Procedures
 args <- commandArgs()
-num_prefix <- length(args) - 5
 prefix <- args[6:length(args)]
-
 XP <- scanXP(prefix)
 save(XP, file=sprintf("%s.XP.Rdata", prefix[1]))
 pdf(sprintf('%s.WG.pdf', prefix[1]))
 amphi_plot( (XP$mjdSec%%86400)/3600, XP$C00, 'Wire Grid C00')
-amphi_plot( (XP$mjdSec%%86400)/3600, XP$C01, 'Wire Grid C01')
 dev.off()
