6b9a58cff3e2d87640bc58b8408ae884b4d22f15
kamenoseiji <seiji.kameno@nao.ac.jp>
2014-09-07 16:23:56 +0900

    modified:   SpecInteg.R

--- SpecInteg.R
+++ SpecInteg.R
@@ -1,11 +1,10 @@
 # SpecInteg.R
-# usage: Rscript ScanPattern.R [Number of SAM45 file] [list of SAM45 file]
+# usage: Rscript SpecInteg.R [Scan.Rdata file name]
 # prefix is YYYYDOYHHMMSS in the PolariS file name (e.g. 2013362105803)
 #
 library(RCurl)
-eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/readSAM45.R", ssl.verifypeer = FALSE)))
 eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/readPolariS.R", ssl.verifypeer = FALSE)))
-eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/Qeff.R", ssl.verifypeer = FALSE)))
+eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/master/mjd.R", ssl.verifypeer = FALSE)))
 setwd('.')
 #-------- Function to produce scan pattern
 scanSegment <- function( mjdSec ){
@@ -62,29 +61,20 @@ R_index   <- which(Scan$scanType == 'R');  RMJD   <- scanSegment(Scan$mjdSec[R_i
 ON_index  <- which(Scan$scanType == 'ON'); onMJD  <- scanSegment(Scan$mjdSec[ON_index])
 OFF_index <- which(Scan$scanType == 'OFF');offMJD <- scanSegment(Scan$mjdSec[OFF_index])
 
-
 #-------- List prefix of PolariS data
-Year <- substr(strsplit(SAM45File[1], '\\.')[[1]][5], 1, 4)
+Year <- mjd2doy(Scan$mjdSec[1])[[1]]
 P00fileList <- system(  sprintf('ls %s*.P.00', Year), intern=T )
 for(index in 1:length(P00fileList)){
 	prefix[index] <- substr(P00fileList[index], 1, 13)
 }
 
-#-------- List IF ID of PolariS data
-P00fileList <- system(  sprintf('ls %s.P.*', prefix[1]), intern=T )
-IF_ID <- integer(0)
-for(index in 1:length(P00fileList)){
-	IF_ID[index] <- as.integer(strsplit(P00fileList[index] , '\\.')[[1]][3])
-}
+#-------- Integrate Segments
+on_C00 <- integSegment(prefix, 'C', 0, onMJD ); off_C00 <- integSegment(prefix, 'C', 0, offMJD ); R_C00 <- integSegment(prefix, 'C', 0, RMJD )
+on_C01 <- integSegment(prefix, 'C', 1, onMJD ); off_C01 <- integSegment(prefix, 'C', 1, offMJD ); R_C01 <- integSegment(prefix, 'C', 1, RMJD )
+on_A00 <- integSegment(prefix, 'A', 0, onMJD ); off_A00 <- integSegment(prefix, 'A', 0, offMJD ); R_A00 <- integSegment(prefix, 'A', 0, RMJD )
+on_A01 <- integSegment(prefix, 'A', 1, onMJD ); off_A01 <- integSegment(prefix, 'A', 1, offMJD ); R_A01 <- integSegment(prefix, 'A', 1, RMJD )
+on_A02 <- integSegment(prefix, 'A', 2, onMJD ); off_A02 <- integSegment(prefix, 'A', 2, offMJD ); R_A02 <- integSegment(prefix, 'A', 2, RMJD )
+on_A03 <- integSegment(prefix, 'A', 3, onMJD ); off_A03 <- integSegment(prefix, 'A', 3, offMJD ); R_A03 <- integSegment(prefix, 'A', 3, RMJD )
 
-#-------- Produce Scan data frame
-for(fileIndex in 1:length(SAM45File)){
-	tempScan <- scanPattern(SAM45File[fileIndex], prefix, IF_ID)
-	if(fileIndex == 1){	Scan <- tempScan}
-	else { Scan <- rbind(Scan, tempScan)}
-}
-#-------- Tsys
-Scan <- scanTsys(Scan, 290.0)
-save(Scan, file=sprintf("%s.Scan.Rdata", prefix[1]))
-
-#-------- Plot
+#-------- Save into file
+save(onMJD, on_C00, on_C01, on_A00, on_A01, on_A02, on_A03, offMJD, off_C00, off_C01, off_A00, off_A01, off_A02, off_A03, RMJD, R_C00, R_C01, R_A00, R_A01, R_A02, R_A03, file='TMC1_SPEC.Rdata')
