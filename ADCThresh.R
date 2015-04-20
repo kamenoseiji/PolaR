b63f68539f14fa8615b3a745a8b210a92254fd7c
SeijiKameno <kameno@ainodake.local>
2015-04-20 17:10:51 -0300

    modified:   ADCThresh.R

--- ADCThresh.R
+++ ADCThresh.R
@@ -9,9 +9,9 @@ eval(parse(text = getURL("https://raw.githubusercontent.com/kamenoseiji/PolaR/ma
 setwd('.')
 
 #-------- Procedures
-#args <- commandArgs(trailingOnly = T)
-args <- c('2015110025209')
-setwd('/Volumes/SSD/PolariS/20150420/')
+args <- commandArgs(trailingOnly = T)
+#args <- c('2015110025209')
+#setwd('/Volumes/SSD/PolariS/20150420/')
 prefix <- args[1]
 
 #-------- List IF ID of BitPower
@@ -25,11 +25,11 @@ LevelNum <- 256
 ThreshNum <- LevelNum - 1
 Thresh <- matrix( nrow=ThreshNum, ncol=length(IF_ID))
 for(if_index in 1:length(IF_ID)){
-	bitDist <- readBitDist(sprintf("%s.P.%02d", prefix, IF_ID[if_index]))
-	thresh <- apply(bitDist, 2, threshLevel)
-	avail_index <- which((colSums(is.infinite(thresh)) + colSums(is.na(thresh))) == 0)
+	levelHisto <- matrix(readBitDist(sprintf("%s.P.%02d", prefix, IF_ID[if_index])), nrow=LevelNum)
+	threshHolds <- apply(levelHisto, 2, threshLevel)
+	avail_index <- which((colSums(is.infinite(threshHolds)) + colSums(is.na(threshHolds))) == 0)
 	if(length(avail_index) == 0){ cat(sprintf("Warning: No available data in %s.P.%02d\n", prefix, IF_ID[if_index]))}
-	Thresh[,if_index] <- apply(thresh[,avail_index], 1, mean)
+	Thresh[,if_index] <- apply(threshHolds[,avail_index], 1, mean)
 }
 fileName <- sprintf("%s.Thresh.Rdata", prefix)
 save(Thresh, file=fileName)
