44995e6ec8532f4b5c5d5754f814b429b9757b0b
SeijiKameno <kameno@ainodake.local>
2015-06-05 19:11:18 -0300

    modified:   PlotZeeman.R

--- PlotZeeman.R
+++ PlotZeeman.R
@@ -8,11 +8,17 @@ parseArg <- function( args ){
     plotFreq <- c(1.6, 1.7)
     smoothCH <- 256
     bunchCH  <- 32
+    restFreq  <- 45379.033000    # CCS rest frequency, [MHz]
+    trackFreq <- 2.0             # Tracking frequency, [MHz]
+    trackVel  <- 5.9             # Tracking velocity,  [km/s]
     for( index in 1:argNum ){
         if(substr(args[index], 1,2) == "-l"){ lineFreq[1] <- as.numeric(substring(args[index], 3))}
         if(substr(args[index], 1,2) == "-L"){ lineFreq[2] <- as.numeric(substring(args[index], 3))}
         if(substr(args[index], 1,2) == "-p"){ plotFreq[1] <- as.numeric(substring(args[index], 3))}
         if(substr(args[index], 1,2) == "-P"){ plotFreq[2] <- as.numeric(substring(args[index], 3))}
+        if(substr(args[index], 1,2) == "-r"){ restFreq <- as.numeric(substring(args[index], 3))}   # Tracking  Frequency
+        if(substr(args[index], 1,2) == "-t"){ trackFreq <- as.numeric(substring(args[index], 3))}     # Reference Frequency
+        if(substr(args[index], 1,2) == "-v"){ trackVel  <- as.numeric(substring(args[index], 3))}     # Reference Frequency
         if(substr(args[index], 1,2) == "-I"){ IF <- as.integer(substring(args[index], 3))}
         if(substr(args[index], 1,2) == "-S"){ srcName <- substring(args[index], 3)}
         if(substr(args[index], 1,2) == "-s"){ smoothCH <- as.integer(substring(args[index], 3))}
@@ -20,7 +26,7 @@ parseArg <- function( args ){
         if(substr(args[index], 1,2) == "-M"){ lineName <- substring(args[index], 3)}
     }
     fileName <- args[argNum]
-    return( list(lineFreq = lineFreq, plotFreq = plotFreq, srcName = srcName, lineName = lineName, fileName = fileName, IF = IF, smoothCH = smoothCH, bunchCH = bunchCH) )
+    return( list(lineFreq = lineFreq, plotFreq = plotFreq, restFreq = restFreq, trackFreq = trackFreq, trackVel = trackVel, srcName = srcName, lineName = lineName, fileName = fileName, IF = IF, smoothCH = smoothCH, bunchCH = bunchCH) )
 }
 
 RPATH <- '~/Programs/PolaR'
@@ -40,20 +46,12 @@ setwd('.')
 args <- parseArg(commandArgs(trailingOnly = T))
 setwd('.')
 load(args$fileName)	 #Load Stokes file
-# freq_track <- 45379033000 # [Hz]
-# freq_center <- c(45490316000, 45379033000, 45490316000, 45379033000)
-# freq_IF <- 5208	# [MHz]
-# freq_1stLO <- freq_track - freq_IF*1e6	#[Hz]
-# freq_BBC <- c(5317.0, 5206.0, 5317.0, 5206.0)
-# freq_rest   <- freq_center
-# velSrc      <- 5900 # m/s
-chNum <- length(StokesI02)
-SDrange <- 8193:16384
-freq <- (0:(chNum-1))/chNum* 4.0	# MHz
-# veloc0 <- (2.0 - freq)*1.0e6 /  freq_center[1] * 299792458 + 
-chSep <- 4.0 / chNum
-#-------- Save into file
 PDFfilename <- sprintf("%s.Zeeman.%d.pdf", strsplit(args$fileName, "\\.")[[1]][1], args$IF)
+chNum <- length(StokesI02)
+freq <- (0:(chNum-1))/chNum* 4.0	# MHz
+chSep <- 4.0 / chNum
+veloc0 <- (args$trackFreq - freq) / args$restFreq * 299792.458 + args$trackVel
+SDrange <- 8193:16384
 if(args$IF == 2){
     StokesI <- StokesI13
     StokesQ <- StokesQ13
@@ -87,32 +85,36 @@ predStokesU <- predict(fitStokesU, freq[plotRange])$y
 fitBunch  <- 8
 cols   <- c('black', 'blue', 'green')
 labels <- c('I', 'Q', 'U')
-#plot( bunch_vec(freq[plotRange],plotBunch)-0.5*plotBunch*chSep, bunch_vec(StokesI[plotRange], plotBunch), type='s', xlab='Frequency [MHz]', ylab='Stokes I [K]', main=sprintf('%s %s', args$srcName, args$lineName))
-plot( freq[plotRange], StokesI[plotRange], type='l', xlab='Frequency [MHz]', ylab='Stokes I [K]', main=sprintf('%s %s', args$srcName, args$lineName))
-lines( bunch_vec(freq[plotRange],plotBunch)-0.5*plotBunch*chSep, bunch_vec(StokesQ[plotRange], plotBunch), type='s', col=cols[2])
-lines( bunch_vec(freq[plotRange],plotBunch)-0.5*plotBunch*chSep, bunch_vec(StokesU[plotRange], plotBunch), type='s', col=cols[3])
-lines( bunch_vec(freq[plotRange], fitBunch), bunch_vec(predStokesI, fitBunch), col='orange')
+#plot( freq[plotRange], StokesI[plotRange], type='l', xlab='Frequency [MHz]', ylab='Stokes I [K]', main=sprintf('%s %s', args$srcName, args$lineName))
+#lines( bunch_vec(freq[plotRange],plotBunch)-0.5*plotBunch*chSep, bunch_vec(StokesQ[plotRange], plotBunch), type='s', col=cols[2])
+#lines( bunch_vec(freq[plotRange],plotBunch)-0.5*plotBunch*chSep, bunch_vec(StokesU[plotRange], plotBunch), type='s', col=cols[3])
+#lines( bunch_vec(freq[plotRange], fitBunch), bunch_vec(predStokesI, fitBunch), col='orange')
+plot( veloc0[plotRange], StokesI[plotRange], type='l', xlab='LSR Velocity [km/s]', ylab='Stokes I [K]', main=sprintf('%s %s', args$srcName, args$lineName))
+lines( bunch_vec(veloc0[plotRange],plotBunch)-0.5*plotBunch*chSep, bunch_vec(StokesQ[plotRange], plotBunch), type='s', col=cols[2])
+lines( bunch_vec(veloc0[plotRange],plotBunch)-0.5*plotBunch*chSep, bunch_vec(StokesU[plotRange], plotBunch), type='s', col=cols[3])
+lines( bunch_vec(veloc0[plotRange], fitBunch), bunch_vec(predStokesI, fitBunch), col='orange')
 legend("topleft", legend=labels, col=cols, lty=rep(1,3))
 abline(h=0, col='gray')
 #-------- Plot dI/df 
-#plot( bunch_vec(freq[plotRange],plotBunch)-0.5*plotBunch*chSep, bunch_vec(predStokesV, plotBunch), type='s', xlab='Frequency [MHz]', ylab='dI/df [K/Hz]', main=sprintf('%s %s', args$srcName, args$lineName), col='red')
-plot(freq[plotRange], predStokesV, type='l', xlab='Frequency [MHz]', ylab='dI/df [K/Hz]', main=sprintf('%s %s', args$srcName, args$lineName), col='red')
-
-maxDif <- max(predStokesV); maxFreq <- freq[plotRange[which.max(predStokesV)]]; cat(sprintf('%8.5e K/Hz @ %5.3f MHz\n', maxDif, maxFreq))
-minDif <- min(predStokesV); minFreq <- freq[plotRange[which.min(predStokesV)]]; cat(sprintf('%8.5e K/Hz @ %5.3f MHz\n', minDif, minFreq)) 
-text( maxFreq, maxDif, sprintf('%5.2e K/Hz at %4.2f MHz', maxDif, maxFreq), cex=0.3, pos=4)
-text( minFreq, minDif, sprintf('%5.2e K/Hz at %4.2f MHz', minDif, minFreq), cex=0.3, pos=4)
+#plot(freq[plotRange], predStokesV, type='l', xlab='Frequency [MHz]', ylab='dI/df [K/Hz]', main=sprintf('%s %s', args$srcName, args$lineName), col='red')
+plot(veloc0[plotRange], predStokesV, type='l', xlab='LSR Velocity [km/s]', ylab='dI/df [K/Hz]', main=sprintf('%s %s', args$srcName, args$lineName), col='red')
+maxDif <- max(predStokesV); maxVeloc <- veloc0[plotRange[which.max(predStokesV)]]; cat(sprintf('%8.5e K/Hz @ %5.3f km/s\n', maxDif, maxVeloc))
+minDif <- min(predStokesV); minVeloc <- veloc0[plotRange[which.min(predStokesV)]]; cat(sprintf('%8.5e K/Hz @ %5.3f km/s\n', minDif, minVeloc)) 
+text( maxVeloc, maxDif, sprintf('%5.2e K/Hz at %4.2f km/s', maxDif, maxVeloc), cex=0.3, pos=4)
+text( minVeloc, minDif, sprintf('%5.2e K/Hz at %4.2f km/s', minDif, minVeloc), cex=0.3, pos=4)
 abline(h=0)
 #-------- Plot Stokes V 
 err <- sd(StokesV02[SDrange]) / sqrt(plotBunch)
 fit <- lm(formula=z~1+x+y, data=data.frame(x=predStokesV[lineRange], y=predStokesI[lineRange], z=StokesV[plotRange[lineRange]]))
 summary(fit)
-plotX <- bunch_vec(freq[plotRange], plotBunch)
+#plotX <- bunch_vec(freq[plotRange], plotBunch)
+plotX <- bunch_vec(veloc0[plotRange], plotBunch)
 plotY <- bunch_vec(StokesV[plotRange] - fit$coefficients[1] - fit$coefficients[3]* predStokesI, plotBunch)
 Ymax <- max(abs(plotY))
-plot( plotX, plotY , pch=20, ylim=c(-2.0*Ymax, 2.0*Ymax), xlab='Frequency [MHz]', ylab='Stokes V [K]', main=sprintf('%s %s', args$srcName, args$lineName))
+plot( plotX, plotY , pch=20, ylim=c(-2.0*Ymax, 2.0*Ymax), xlab='LSR Velocity [km/s]', ylab='Stokes V [K]', main=sprintf('%s %s', args$srcName, args$lineName))
 arrows( plotX, plotY - err, plotX, plotY + err, angle=90, length=0)
-lines( bunch_vec(freq[plotRange], fitBunch), bunch_vec(predStokesV*fit$coefficients[2], fitBunch), col='red')
-legend(min(freq[plotRange]), 1.9*Ymax, legend=sprintf('Zeeman Shift = %5.1f ± %4.1f Hz', fit[[1]][2], summary(fit)[[4]][5]))
+#lines( bunch_vec(freq[plotRange], fitBunch), bunch_vec(predStokesV*fit$coefficients[2], fitBunch), col='red')
+lines( bunch_vec(veloc0[plotRange], fitBunch), bunch_vec(predStokesV*fit$coefficients[2], fitBunch), col='red')
+legend(min(veloc0[plotRange]), 1.9*Ymax, legend=sprintf('Zeeman Shift = %5.1f ± %4.1f Hz', fit[[1]][2], summary(fit)[[4]][5]))
 
 dev.off()
